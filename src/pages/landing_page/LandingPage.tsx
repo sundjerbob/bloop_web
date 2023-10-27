78e40f68e65667736513d61f2404a800232417d5
Tadija <tadijasimic999@gmail.com>
2023-10-27 04:55:31 +0200

    z

--- src/pages/landing_page/LandingPage.tsx
+++ src/pages/landing_page/LandingPage.tsx
@@ -1,15 +1,33 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './style/LandingPage.css'
 import AnimatedConsole from "../../components/animated_console/AnimatedConsole.tsx";
 import HeroSection from "../../components/hero_section/HeroSection.tsx";
 
 const LandingPage: React.FC = () => {
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const slider = document.querySelector('.slider');
+
+    const handleScroll = () => {
+        // Update scrollPosition state with the current scroll position
+        setScrollPosition(window.scrollY);
+        slider?.setAttribute('--scroll-position', scrollPosition.toString());
+    };
+
     useEffect(() => {
+
+        window.addEventListener('scroll', handleScroll);
+
         setTimeout(() => {
             (document.querySelector('.content-holder') as HTMLElement).classList.add('init');
         }, 1)
 
-    })
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, []);
 
 
     const code = [
@@ -30,13 +48,18 @@ const LandingPage: React.FC = () => {
         <>
 
             <div className="content-holder">
-                <AnimatedConsole code={code}/>
-
+                <div className="bg"/>
                 <div className="slider"/>
+
                 <HeroSection/>
 
 
+                <AnimatedConsole code={code}/>
+
+
+
             </div>
+
         </>
     );
 }
